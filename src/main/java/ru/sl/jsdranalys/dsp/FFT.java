a36717744e2c17f751ca959bce46a70c0eb27456
user <user@WIN-QN60THQ3BRV>
2024-10-10 11:02:00 +0300

    Разработка FFT

--- src/main/java/ru/sl/jsdranalys/dsp/FFT.java
+++ src/main/java/ru/sl/jsdranalys/dsp/FFT.java
@@ -3,19 +3,40 @@ package ru.sl.jsdranalys.dsp;
 import java.util.Arrays;
 
 public class FFT {
-	
-	//шаг 4-х точечного БПФ
+
+	// шаг 4-х точечного БПФ
 	private static int RSTEP = 4;
-	//индексы массивов для вычисления комлексных значений
+	// индексы массивов для вычисления комлексных значений
 	private static int IA = 0;
 	private static int IB = 1;
 	private static int IC = 2;
 	private static int ID = 3;
-	
-	FFT(){
-		
+	private static double [] EXP2N_SIN = new double [63];
+	private static double [] EXP2N_COS = new double [63];
+	private static int [] MASK = new int [63];
+
+
+	FFT() {
+		//Инициализация расчетных данных
+		int k = 1;
+		for (int i = 0; i < 32; i++) {
+			double arg = 2*Math.PI/k;
+			EXP2N_SIN[i] = -Math.sin(arg);
+			EXP2N_COS[i] = Math.cos(arg);
+			MASK[i] = k;
+			k <<= 1;
+		}
+		double old = EXP2N_SIN[31];
+		for (int i = 32; i < 63; i++) {
+			EXP2N_SIN[i] = old/2;
+			old = EXP2N_SIN[i];
+			EXP2N_COS[i] = 1;
+			MASK[i] = k;
+			k <<= 1;
+		}
+
 	}
-	
+
 	/*
 	 * БПФ по основанию 2
 	 * 
@@ -27,8 +48,8 @@ public class FFT {
 	 *          /      \
 	 * [bi , br]        [ar-br , ai-bi]
 	 */
-	private static void calcTwoValues (final double[] rValues, final double[] iValues){
-		
+	private static void calcTwoValues(final double[] rValues, final double[] iValues) {
+
 		final double rA = rValues[IA];
 		final double iA = iValues[IA];
 		final double rB = rValues[IB];
@@ -52,25 +73,60 @@ public class FFT {
 	 *    /       \
 	 *  D           A+jB-C-jD
 	 */
-	private static void calcFourValues (final double[] rValues, final double[] iValues, int pos){
-		
+	private static void calcFourValues(final double[] rValues, final double[] iValues, int pos) {
+
 		final double[] rV = Arrays.copyOfRange(rValues, pos, pos + RSTEP);
 		final double[] iV = Arrays.copyOfRange(iValues, pos, pos + RSTEP);
-		
-		//A+B+C+D
-		rValues[pos + IA] = rV[IA] + rV[IB] + rV[IC] + rV[ID];
+		// Для ускорения работы оперфции с комплексными числами вынесены в
+		// отдельный метод
+		rValues[pos + IA] = rV[IA] + rV[IB] + rV[IC] + rV[ID];// A+B+C+D
 		iValues[pos + IA] = iV[IA] + iV[IB] + iV[IC] + iV[ID];
-		//A-jB-C+jD
-		rValues[pos + IB] = rV[IA] + iV[IB] - rV[IC] - iV[ID];
+		rValues[pos + IB] = rV[IA] + iV[IB] - rV[IC] - iV[ID];// A-jB-C+jD
 		iValues[pos + IB] = iV[IA] - rV[IB] - iV[IC] + rV[ID];
-		//A-B+C-D
-		rValues[pos + IC] = rV[IA] - rV[IB] + rV[IC] - rV[ID];
+		rValues[pos + IC] = rV[IA] - rV[IB] + rV[IC] - rV[ID];// A-B+C-D
 		iValues[pos + IC] = iV[IA] - iV[IB] + iV[IC] - iV[ID];
-		//A+jB-C-jD
-		rValues[pos + ID] = rV[IA] - iV[IB] - rV[IC] + iV[ID];
-		iValues[pos + ID] = iV[IA] + rV[IB] - iV[IC] - rV[ID];	
+		rValues[pos + ID] = rV[IA] - iV[IB] - rV[IC] + iV[ID];// A+jB-C-jD
+		iValues[pos + ID] = iV[IA] + rV[IB] - iV[IC] - rV[ID];
 	}
 	
+	public void calculate(double [] reData, double [] imData) {
+		
+		int arrCount = reData.length;
+		//реверс значений (зеркальный бинарный поворот индекса)
+		int pow = MathUtilites.pow2(arrCount-1);		
+		for (int i = 0; i < arrCount >> 1; i++) {
+			int curr = i;
+			int out = 0;
+			int mask = MASK[pow-1];
+			for (int j = 0; j < pow; j++) {
+				if ((curr & 1) == 1)
+					out += mask;
+				mask >>= 1;
+				curr >>= 1;
+			}
+			double temp = reData[i];
+			reData[i] = reData[out];
+			reData[out] = temp;
+			temp = imData[i];
+			imData[i] = imData[out];
+			imData[out] = temp;
+		}
+		//расчет "бабочек"
+		for (int i = 0; i < arrCount; i += RSTEP) {
+			calcFourValues(reData, imData, i);
+		}
+		
+		
+	}
+
+	public static void main(String[] args) {
+		double [] re = {1,2,3,4,5,6,7,8};
+		double [] im = {1,2,3,4,5,6,7,8};
+		FFT fft = new FFT();
+		fft.calculate(re, im);
+		System.out.println(Arrays.toString(re));
+		System.out.println(Arrays.toString(im));
 	
+	}
 
 }
--- src/main/java/ru/sl/jsdranalys/dsp/MathUtilites.java
+++ src/main/java/ru/sl/jsdranalys/dsp/MathUtilites.java
@@ -4,11 +4,6 @@ import java.util.stream.IntStream;
 
 public class MathUtilites {
 	
-	private static int [] MASK = {0x0,0x1,0x2,0x4,0x8,0x10,0x20,0x40,0x80,
-								  0x100,0x200,0x400,0x800,0x1000,0x2000,0x4000,0x8000,
-								  0x10000,0x20000,0x40000,0x80000,0x100000,0x200000,0x400000,0x800000,
-								  0x1000000,0x2000000,0x4000000,0x8000000,
-								  0x10000000,0x20000000,0x40000000,0x80000000};
 
 	/*
 	 * предполагается: класс Utilites для размещения статических методов с
@@ -31,24 +26,6 @@ public class MathUtilites {
 		return res + 1;
 	}
 	
-	// изменение порядка бит для БПФ
-	public static int[] countBitMirror(int n) {
-		int[] baseArr = IntStream.range(0, n).toArray();
-		int pow = pow2(n-1);		
-		for (int i = 0; i < baseArr.length; i++) {
-			int curr = baseArr[i];
-			int out = 0;
-			int mask = MASK[pow];
-			for (int j = 0; j < pow; j++) {
-				if ((curr & 1) == 1)
-					out += mask;
-				mask >>= 1;
-				curr >>= 1;
-			}
-			baseArr[i] = out;
-		}
-		return baseArr;
-	}
 
 	// вывод 32-х разрядного числа в бинарном виде
 	public static void binPrint(int num) {
